35ded705e7fa9c0719f8191daa760bb17683704d
knknkn1162 <knknkn1162@gmail.com>
2017-10-10 08:01:48 +0900

    add variable example

---
+++ src/primitives/prim.rs
@@ -0,0 +1,20 @@
+pub fn test() {
+    let logical: bool = true;
+
+    let a_float: f64 = 1.0;
+    let an_integer = 5i32;
+
+    let default_float = 3.0;
+    let default_integer = 7;
+
+    let mut inferred_type = 12;
+    inferred_type = 39383474940;
+    //inferred_type = 5.5;
+
+    let mut mutable = 12;
+    mutable = 21;
+
+    //mutable = true;
+
+
+}
