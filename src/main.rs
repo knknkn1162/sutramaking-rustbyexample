5ae6f5d9d01c14c9f9c339e492ec15b4a1076514
knknkn1162 <knknkn1162@gmail.com>
2017-10-17 23:31:13 +0900

    sample iterator::any

--- src/main.rs
+++ src/main.rs
@@ -1,53 +1,10 @@
-mod hello;
-mod primitives;
-mod custom_type;
-mod flow_control;
-mod destructuring;
+//mod hello;
+//mod primitives;
+//mod custom_type;
+//mod flow_control;
+//mod destructuring;
+mod std_lib;
 
 fn main() {
-    //hello::formatted::test();
-    //hello::debug::test01();
-    //hello::debug::test02();
-
-    //hello::disp::test();
-    //hello::testcase::test();
-
-    //hello::formatting::test();
-
-    //primitives::prim::test()
-    // primitives::tuple::test();
-    //primitives::arr_slice::test();
-    //custom_type::structures::test();
-    //custom_type::enums::test();
-    //custom_type::uses::test();
-    //custom_type::clike::test();
-    //custom_type::linked_list::test();
-    //custom_type::constants::test();
-    //custom_type::variable_bindings::test();
-    //custom_type::mutability::test();
-    //custom_type::literals::test();
-    //custom_type::inference::test();
-    //custom_type::aliasing::test();
-    //custom_type::expressions::test();
-    //flow_control::ifelse::test();
-    //flow_control::returning_from_loops::test();
-    // flow_control::whil::test();
-    //flow_control::fo::test();
-    //flow_control::mtch::test();
-    //destructuring::tuples::test();
-    //destructuring::enums::test();
-    //destructuring::pointers_ref::test();
-    //destructuring::structs::test();
-    //destructuring::guards::test();
-    //destructuring::binding::test();
-    // destructuring::iflet::test();
-    //destructuring::while_let::test();
-    //destructuring::functions::test();
-    //destructuring::methods::test();
-    //destructuring::closures::test();
-    //destructuring::capturing::test();
-    //destructuring::input_para::test();
-    //destructuring::type_anonymity::test();
-    //destructuring::input_functions::test();
-    destructuring::output_parameters::test();
+    std_lib::any::test();
 }
