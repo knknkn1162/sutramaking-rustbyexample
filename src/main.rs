cf624ed05bd50380e5cc37bd3a9130e970e75665
knknkn1162 <knknkn1162@gmail.com>
2017-10-18 10:06:47 +0900

    sample visibility

--- src/main.rs
+++ src/main.rs
@@ -4,9 +4,11 @@
 //mod flow_control;
 //mod destructuring;
 mod std_lib;
+mod modules;
 
 fn main() {
     //std_lib::any::test();
     //std_lib::find::test();
-    std_lib::higher_order::test();
+    //std_lib::higher_order::test();
+    modules::visibility::test();
 }
