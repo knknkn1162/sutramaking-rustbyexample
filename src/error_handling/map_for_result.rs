0e58034c70e090b8a0e51d135f6f589e2175b72f
knknkn1162 <knknkn1162@gmail.com>
2017-10-22 15:31:53 +0900

    use type

--- src/error_handling/map_for_result.rs
+++ src/error_handling/map_for_result.rs
@@ -1,13 +1,15 @@
 use std::num::ParseIntError;
 
-fn multiply(first_number_str: &str, second_number_str: &str) -> Result<i32, ParseIntError> {
+type Res<T> = Result<T, ParseIntError>;
+
+fn multiply(first_number_str: &str, second_number_str: &str) -> Res<i32> {
     first_number_str.parse::<i32>().and_then(|first_number| {
         // lambda always success
         second_number_str.parse::<i32>().map(|second_number| first_number * second_number)
     })
 }
 
-fn print(result: Result<i32, ParseIntError>) {
+fn print(result: Res<i32>) {
     match result {
         Ok(n) => println!("n is {}", n),
         Err(e) => println!("Error: {}", e),
