650151cd26ff840b2c98dbeacb866cea138395a9
knknkn1162 <knknkn1162@gmail.com>
2017-10-22 16:19:10 +0900

    comment result above code

--- src/error_handling/option_result.rs
+++ src/error_handling/option_result.rs
@@ -1,5 +1,5 @@
 use std::num::ParseIntError;
-
+// delay ParseIntError
 fn double_first_v1(vec: &Vec<&str>)-> Option<Result<i32, ParseIntError>> {
     // vec.first(&self) -> Option<&T>
     vec.first().map(|first| first.parse::<i32>().map(
@@ -7,6 +7,7 @@ fn double_first_v1(vec: &Vec<&str>)-> Option<Result<i32, ParseIntError>> {
     )
 }
 
+// raise ParseIntError or Ok(None)
 fn double_first_v2(vec: &Vec<&str>)-> Result<Option<i32>, ParseIntError> {
     // type => Option<Result<i32>, ParseIntError>
     let opt = vec.first().map(
@@ -25,12 +26,19 @@ pub fn test() {
     let numbers = vec!["42", "93", "18"];
     let empty = vec![];
     let strings = vec!["tofu", "93", "18"];
-
+    // The first doubled is Some(Ok(84))
     println!("The first doubled is {:?}", double_first_v1(&numbers));
+    // The first doubled is None
     println!("The first doubled is {:?}", double_first_v1(&empty));
+
+    // The first doubled is Some(Err(ParseIntError { kind: InvalidDigit }))
     println!("The first doubled is {:?}", double_first_v1(&strings));
 
+    //The first doubled is Ok(Some(84))
     println!("The first doubled is {:?}", double_first_v2(&numbers));
+
+    // The first doubled is Ok(None)
     println!("The first doubled is {:?}", double_first_v2(&empty));
+    // The first doubled is Err(ParseIntError { kind: InvalidDigit })
     println!("The first doubled is {:?}", double_first_v2(&strings));
 }
